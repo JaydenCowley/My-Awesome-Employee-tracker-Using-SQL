16d2e06a09b9dfde6e7de515273dc2de13b38d7c
Jayden Cowley <jcowley16@gmail.com>
2022-08-09 13:28:46 -0600

    Adding more seed data to employee table

--- seeds/seed_db.sql
+++ seeds/seed_db.sql
@@ -16,4 +16,8 @@ VALUES
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-("JAYDEN", "COWLEY", 20, )
+("JAYDEN", "COWLEY", 20, 2),
+("BRAD", "SMITH", 40, 1),
+("COREY", "WEBB", 50, 2),
+("CHRISTIAN", "THOMAS", 30, 1)
+("DEVIN", "BRANDON", 10, 1)
