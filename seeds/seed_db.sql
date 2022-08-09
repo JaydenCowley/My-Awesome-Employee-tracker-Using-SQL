8938045c8babf5ccf426d867e3b9e21dbbfc38c0
Jayden Cowley <jcowley16@gmail.com>
2022-08-08 19:01:06 -0600

    added seed data for department and role and started on employee

---
+++ seeds/seed_db.sql
@@ -0,0 +1,19 @@
+INSERT INTO department(id, name)
+VALUES
+    (1, Marketing),
+    (2, IT),
+    (3, Graphics),
+    (4, Production),
+    (5, Warhouse);
+
+INSERT INTO role(id, name, salary, department_id)
+VALUES
+    (10, Leadership),
+    (20, Developer),
+    (30, Artist),
+    (40, Operator),
+    (50, Loader);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+("JAYDEN", "COWLEY", 20, )
