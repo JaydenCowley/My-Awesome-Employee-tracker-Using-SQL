96f869577b40f0dff53c016dcd40ef44967d53ac
Jayden Cowley <jcowley16@gmail.com>
2022-08-10 18:37:54 -0600

    updating seeds

--- db/schema.sql
+++ db/schema.sql
@@ -8,15 +8,15 @@ CREATE TABLE department(
     name VARCHAR(30)
     );
 CREATE TABLE role(
-    id PRIMARY KEY,
-    name VARCHAR(30),
+    id  INT PRIMARY KEY,
+    title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER
 );
 CREATE TABLE employee(
-    id INTEGER PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER NULL
 );
--- seeds/seed_db.sql
+++ seeds/seed_db.sql
@@ -6,18 +6,18 @@ VALUES
     (4, Production),
     (5, Warhouse);
 
-INSERT INTO role(id, name, salary, department_id)
+INSERT INTO role(id, title, salary, department_id)
 VALUES
-    (10, Leadership),
-    (20, Developer),
-    (30, Artist),
-    (40, Operator),
-    (50, Loader);
+    (10, Leadership, 40000, 1),
+    (20, Developer, 80000, 2),
+    (30, Artist, 30000, 3),
+    (40, Operator, 45000, 4),
+    (50, Loader, 50000, 5);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
 ("JAYDEN", "COWLEY", 20, 2),
 ("BRAD", "SMITH", 40, 1),
 ("COREY", "WEBB", 50, 2),
-("CHRISTIAN", "THOMAS", 30, 1)
-("DEVIN", "BRANDON", 10, 1)
+("CHRISTIAN", "THOMAS", 30, 1),
+("DEVIN", "BRANDON", 10, 1);
