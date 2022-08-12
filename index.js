0a2bdaf3069a31d76be92905d97ea183bbc9ae1c
Jayden Cowley <jcowley16@gmail.com>
2022-08-11 18:00:46 -0600

    Adding if statements for the viewing options

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import inquirer from "inquirer"
 
+const departments = department.name;
+const roles = role.title;
+const employees = employees.first_name;
+
+const cTable = require('console.table');
 function startPrompt() {
     inquirer.prompt([
      {
@@ -10,6 +15,16 @@ function startPrompt() {
             'View all departments', 'view all roles', 'view all employees', new inquirer.Separator('---Add---'), 
             'Add a department', 'add a role', 'add an employee', 'update and employee role',new inquirer.Separator('---view---')]
      }   
-    ])
+    ]).then((answer) => {
+        if (answer = 'view all departments') {
+            console.table(['Departments'], departments)
+        }
+        if (answer = 'view all roles') {
+            console.table(['Roles'], roles)
+        }
+        if (answer = 'view all employees') {
+            console.table(['Employees'], employees)
+        }
+    })
 }
 startPrompt();
--- seeds/seed_db.sql
+++ seeds/seed_db.sql
@@ -1,23 +1,23 @@
 INSERT INTO department(id, name)
 VALUES
-    (1, Marketing),
-    (2, IT),
-    (3, Graphics),
-    (4, Production),
-    (5, Warhouse);
+    (1, 'Marketing'),
+    (2, 'IT'),
+    (3, 'Graphics'),
+    (4, 'Production'),
+    (5, 'Warhouse');
 
 INSERT INTO role(id, title, salary, department_id)
 VALUES
-    (10, Leadership, 40000, 1),
-    (20, Developer, 80000, 2),
-    (30, Artist, 30000, 3),
-    (40, Operator, 45000, 4),
-    (50, Loader, 50000, 5);
+    (10, 'Leadership', 40000, 1),
+    (20, 'Developer', 80000, 2),
+    (30, 'Artist', 30000, 3),
+    (40, 'Operator', 45000, 4),
+    (50, 'Loader', 50000, 5);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-("JAYDEN", "COWLEY", 20, 2),
-("BRAD", "SMITH", 40, 1),
-("COREY", "WEBB", 50, 2),
-("CHRISTIAN", "THOMAS", 30, 1),
-("DEVIN", "BRANDON", 10, 1);
+('JAYDEN', 'COWLEY', 20, 2),
+('BRAD', 'SMITH', 40, 1),
+('COREY', 'WEBB', 50, 2),
+('CHRISTIAN', 'THOMAS', 30, 1),
+('DEVIN', 'BRANDON', 10, 1);
